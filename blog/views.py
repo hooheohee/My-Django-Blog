17fb6e92c4881b63f77973677caa111eff8c384b
Yue Zheng <zheng.yue1@husky.neu.edu>
2019-05-03 12:56:26 -0400

    asd

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog post 1',
+        'content': 'First blog content',
+        'date_posted': 'August 10, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog post 2',
+        'content': 'Second blog content',
+        'date_posted': 'August 20, 2018'
+    }
+]
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
